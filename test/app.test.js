e2193f3f1c7f8a4088a80a8b9e8cac464ea8e11b
Louis Martin-Deprez <louismart00@gmail.com>
2024-01-08 12:28:29 +0100

    Basket Feature

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,4 @@
-const Cube = require('../src/app').Cube;
+const {Cube,Panier} = require('../src/app');
 const expect = require('chai').expect;
 
 describe('Testing the Cube Functions', function() {
@@ -21,3 +21,36 @@ describe('Testing the Cube Functions', function() {
     });
     
 });
+
+describe('Basket Class', () => {
+    let basket;
+    let cube1;
+    let cube2;
+  
+    beforeEach(() => {
+      basket = new Panier();
+      cube1 = new Cube(2, 10);
+      cube2 = new Cube(3, 15);
+    });
+  
+    it('should add a cube to the basket', () => {
+      basket.addCube(cube1);
+      expect(basket.basket).to.have.lengthOf(1);
+      expect(basket.basket[0]).to.deep.equal(cube1);
+    });
+  
+    it('should remove a cube from the basket', () => {
+      basket.addCube(cube1);
+      basket.addCube(cube2);
+      basket.removeCube(cube1);
+      expect(basket.basket).to.have.lengthOf(1);
+      expect(basket.basket[0]).to.equal(cube2);
+    });
+  
+    it('should calculate the total price of cubes in the basket', () => {
+      basket.addCube(cube1);
+      basket.addCube(cube2);
+      const totalPrice = basket.calculateTotalPrice();
+      expect(totalPrice).to.equal(10 + 15);
+    });
+  });
